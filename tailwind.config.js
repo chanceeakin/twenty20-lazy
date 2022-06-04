ea43752c466ea564803c436ae9369ea5fbeb2094
Chance Eakin <chance.eakin@gmail.com>
2022-06-03 19:05:35 -0500

    added css purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,15 @@ module.exports = {
   },
   variants: {},
   plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "components/**/*.js",
+      "layouts/**/*.js",
+      "pages/**/*.js",
+      "plugins/**/*.js",
+      "nuxt.config.js",
+    ],
+  },
 }
