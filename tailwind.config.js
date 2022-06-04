032a75df9be03777f955f3c07028a4afc70a4934
Chance Eakin <chance.eakin@gmail.com>
2022-06-03 19:02:05 -0500

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   theme: {
-    colors: {
-      home: "#283618",
-      about: "#AC6C25",
-      links: "#606C38",
+    extend: {
+      colors: {
+        home: "#283618",
+        about: "#AC6C25",
+        links: "#606C38",
+      },
     },
-    extend: {},
   },
   variants: {},
   plugins: [],
